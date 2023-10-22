05ac5d1eb602223f831f687624c421e6a5a7c7ab
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-10-22 15:34:11 -0700

    Finish MVP.

--- day1/answer.go
+++ day1/answer.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func ReadFileLines(fileName string) []string {
@@ -23,10 +24,29 @@ func ReadFileLines(fileName string) []string {
 	return fileLines
 }
 
-func main() {
-	fmt.Printf("Reading input.txt...\n")
-	lines := ReadFileLines("input.txt")
-	for i := 0; i < len(lines); i++ {
-		fmt.Printf(lines[i] + "\n")
+func GetMostCalories(fileLines []string) int {
+	largest := 0
+	currentSum := 0
+	for i := 0; i < len(fileLines); i++ {
+		if fileLines[i] == "" {
+			if largest < currentSum {
+				largest = currentSum
+			}
+			currentSum = 0
+		} else {
+			lineAmount, err := strconv.Atoi(fileLines[i])
+			if err != nil {
+				panic(err)
+			}
+			currentSum = currentSum + lineAmount
+		}
 	}
+	return largest
+}
+
+func main() {
+	fmt.Printf("Reading input.txt...\n\n")
+	lines := ReadFileLines("input.txt")
+	mostCals := strconv.Itoa(GetMostCalories(lines))
+	fmt.Printf("The elf carrying the most calories has:\n" + mostCals + " calories\n")
 }
