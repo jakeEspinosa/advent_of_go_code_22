41c89f150a6e0c2c271e44d4d4190b4591fd9afb
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-10-28 19:01:54 -0700

    Conversion func.

--- day1/answer2.go
+++ day1/answer2.go
@@ -1,36 +1,23 @@
 package main
 
-// import (
-// 	"strconv"
-// )
+import (
+	"strconv"
+)
 
 // Strategy:
-// convert the string arr to an int arr
 // sort the int arr in reverse order
 // get the first 3 elements, sum them, and return
 
-// func GetTopThree(fileLines []string) int {
-// 	topThreeArray := [3]int{0, 0, 0}
+func ConvertStringArrayToIntArray(stringArray []string) []int {
+	var intSlice []int
+	for i := 0; i < len(stringArray); i++ {
+		newInt, err := strconv.Atoi(stringArray[i])
+		if err != nil {
+			panic(err)
+		}
 
-// 	currentSum := 0
-// 	for i := 0; i < len(fileLines); i++ {
-// 		if fileLines[i] == "" {
-// 			if topThreeArray[0] < currentSum {
-// 				topThreeArray[0] = currentSum
-// 			} else if topThreeArray[1] < currentSum {
-// 				topThreeArray[1] = currentSum
-// 			} else if topThreeArray[2] < currentSum {
-// 				topThreeArray[2] = currentSum
-// 			}
-// 			currentSum = 0
+		intSlice = append(intSlice, newInt)
+	}
 
-// 		} else {
-// 			lineAmount, err := strconv.Atoi(fileLines[i])
-// 			if err != nil {
-// 				panic(err)
-// 			}
-// 			currentSum = currentSum + lineAmount
-// 		}
-// 	}
-// 	return topThreeArray[0] + topThreeArray[1] + topThreeArray[2]
-// }
+	return intSlice
+}
