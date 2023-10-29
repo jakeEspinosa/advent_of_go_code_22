4b524408d02875beb76c8bedbaec2bb80a86ae99
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-10-28 19:42:52 -0700

    Begin refactoring.

--- day1/answer1.go
+++ day1/answer1.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -22,6 +23,33 @@ func ReadFileLines(fileName string) []string {
 
 	file.Close()
 	return fileLines
+}
+
+func SumStringArray(stringArray []string) []int {
+	var result []int
+
+	currentSum := 0
+	for i := 0; i < len(stringArray); i++ {
+		if stringArray[i] == "" {
+			result = append(result, currentSum)
+			currentSum = 0
+
+		} else {
+			lineAmount, err := strconv.Atoi(stringArray[i])
+			if err != nil {
+				panic(err)
+			}
+			currentSum += lineAmount
+		}
+	}
+
+	return result
+}
+
+func SortAndReturnTotal(intArray []int) int {
+	sort.Sort(sort.Reverse(sort.IntSlice(intArray)))
+	total := intArray[0] + intArray[1] + intArray[2]
+	return total
 }
 
 func GetMostCalories(fileLines []string) int {
